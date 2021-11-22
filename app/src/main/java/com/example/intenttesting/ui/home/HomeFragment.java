3cb97d5563faa9cb8015228a3c09cb5db4cfbcf3
Prasannjeet Singh <Prasannjeet.Singh@ist.com>
2021-11-22 03:24:34 +0100

    Successfully completed the app fragment that is able to send an email Intent.

--- app/src/main/java/com/example/intenttesting/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/intenttesting/ui/home/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.example.intenttesting.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,8 +16,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.intenttesting.MainActivity;
 import com.example.intenttesting.R;
 import com.example.intenttesting.databinding.FragmentHomeBinding;
+
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
@@ -28,13 +35,30 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        Button sendButton = binding.sendButton;
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                 String email = binding.emailAddress.getText().toString();
+                 String subject = binding.subject.getText().toString();
+                 String message = binding.message.getText().toString();
+
+                 String[] addresses = email.split(",");
+
+                 Intent intent = new Intent(Intent.ACTION_SENDTO);
+                 intent.setData(Uri.parse("mailto:"));
+                 intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                 intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                 intent.putExtra(Intent.EXTRA_TEXT, message);
+
+                 if (intent.resolveActivity(requireContext().getPackageManager()) != null) {
+                     startActivity(intent);
+                 } else {
+                     Toast.makeText(getActivity(), "No App is Installed", Toast.LENGTH_SHORT).show();
+                 }
             }
         });
+
         return root;
     }
 
